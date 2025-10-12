49bd3a03c8b475f04ac2c8fcd13a6263051e242c
Mohammad Maroof <mmaroof487@gmail.com>
2025-10-12 21:14:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: process.env.CLIENT_URL || "https://athletiq-frontend.onrender.com/register",
+		origin: "https://athletiq-frontend.onrender.com/register",
 		credentials: true,
 	})
 );
@@ -27,4 +27,5 @@ app.use("/api/v1/user", authMiddleware, userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
