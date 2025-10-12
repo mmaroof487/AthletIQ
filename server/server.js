b0f412ce09725a1c5cdcaf94e962a213c4a7d98d
Mohammad Maroof <mmaroof487@gmail.com>
2025-10-12 21:19:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ import authMiddleware from "./middlewares/authMiddleware.js";
 dotenv.config();
 const app = express();
 
-app.use(
-	cors({
-		origin: "https://athletiq-frontend.onrender.com",
-		credentials: true,
-	})
-);
+app.use(cors({
+  origin: 'https://athletiq-frontend.onrender.com', 
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true, 
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -27,6 +27,7 @@ app.use("/api/v1/user", authMiddleware, userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
