9c8e91fc1e97ba55a226629b9327983d9ce0ee98
mmaroof487 <mmaroof487gmail.com git config --global user.name mmaroof487git config --global user.email mmaroof487gmail.com>
2026-02-17 14:46:39 +0530

    final

--- server/config/db.js
+++ server/config/db.js
@@ -11,8 +11,6 @@ dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 if (!process.env.DATABASE_URL) {
 	console.error("DATABASE_URL is not defined in environment variables!");
-} else {
-	console.log("Connecting to database on port:", process.env.DATABASE_URL.split(":").pop().split("/")[0]);
 }
 
 const client = new Client({
@@ -26,8 +24,6 @@ client
 	.catch((err) => {
 		console.error("Database connection failed!");
 		console.error("Error Code:", err.code);
-		console.error("Address:", err.address);
-		console.error("Port:", err.port);
 		console.error("Detail:", err.message);
 	});
 
